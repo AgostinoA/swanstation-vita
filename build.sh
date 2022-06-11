7bb5cc65ae239c19f2db736c5fe488782138972e
Fixer <www.ago.91@gmail.com>
2022-06-11 22:10:12 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,5 @@ export LD_LIBRARY_PATH="${HERE}/vendor/vitasdk/arm-vita-eabi/lib"
 export LD_="$HERE/vendor/vitasdk/bin":"$HERE/vendor/vitasdk/arm-vita-eabi/bin":"${PATH}"
 export PATH="$HERE/vendor/vitasdk/bin":"${PATH}"
 
-export LD=arm-vita-eabi-ld
-export CC=arm-vita-eabi-gcc
-export CXX=arm-vita-eabi-g++
 cmake .
 make
