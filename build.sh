1121c7af0213d9781e8f6bcba4c84822fdc84778
Fixer <www.ago.91@gmail.com>
2022-06-11 22:10:40 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 export HERE="$(dirname "$(readlink -f "${0}")")"
 export VITASDK="$HERE/vendor/vitasdk"
-export LD_LIBRARY_PATH="${HERE}/vendor/vitasdk/arm-vita-eabi/lib"
-export LD_="$HERE/vendor/vitasdk/bin":"$HERE/vendor/vitasdk/arm-vita-eabi/bin":"${PATH}"
 export PATH="$HERE/vendor/vitasdk/bin":"${PATH}"
 
 cmake .
